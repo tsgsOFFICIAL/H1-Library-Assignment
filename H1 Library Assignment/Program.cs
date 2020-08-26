afd80094a690c064b0268404e2105c5e96e12229
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-26 11:55:55 +0200

    Add project files.

---
+++ H1 Library Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H1_Library_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
