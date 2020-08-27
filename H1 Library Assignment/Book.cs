c60d87e141263aec5ea443bd327903875fe88942
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-27 10:15:48 +0200

    books

--- H1 Library Assignment/Book.cs
+++ H1 Library Assignment/Book.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace H1_Library_Assignment
 {
     public class Book
     {
         #region Fields
+        private static uint idCounter;
         private string name;
         private string author;
-        private DateTime releaseDate;
         private bool available;
+        private uint id;
+        private DateTime releaseDate;
 
         #endregion
 
@@ -25,6 +28,17 @@ namespace H1_Library_Assignment
         }
 
         /// <summary>
+        /// This is the name of the author
+        /// </summary>
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+        }
+
+        /// <summary>
         /// This shows if the book is availabe to borrow at the library
         /// </summary>
         public bool Available
@@ -32,6 +46,43 @@ namespace H1_Library_Assignment
             get
             {
                 return available;
+            }
+            set
+            {
+                available = value;
+            }
+        }
+
+        /// <summary>
+        /// This is the ID of the book, each book has their own individual id
+        /// </summary>
+        public uint ID
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// This is the release date of the book
+        /// </summary>
+        public DateTime ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+            }
+        }
+
+        /// <summary>
+        /// This is the total amount of books
+        /// </summary>
+        public uint Count
+        {
+            get
+            {
+                return idCounter;
             }
         }
         #endregion
@@ -45,6 +96,8 @@ namespace H1_Library_Assignment
         /// <param name="releaseDate">This is the releasedate of the book</param>
         public Book(string name, string author, DateTime releaseDate)
         {
+            idCounter++;
+            id = idCounter;
             this.name = name;
             this.author = author;
             this.releaseDate = releaseDate;
--- H1 Library Assignment/Program.cs
+++ H1 Library Assignment/Program.cs
@@ -6,9 +6,12 @@ namespace H1_Library_Assignment
 {
     class Program
     {
+        static List<Book> books = new List<Book>(); //Initialise a new list of Books
+        static List<Book> booksBorrowed = new List<Book>();
         static void Main(string[] args)
         {
-            string[] msg = new string[] 
+            #region Welcome message
+            string[] msg = new string[]
             {
                 @" /$$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$  /$$          /$$$  ",
                 @"| $$__  $$ /$$__  $$ /$$__  $$| $$  /$$/ /$$__  $$| $$         |_  $$ ",
@@ -24,12 +27,325 @@ namespace H1_Library_Assignment
             {
                 Console.WriteLine(line);
             }
+            System.Threading.Thread.Sleep(1500);
+            #endregion
 
-            List<Book> books = new List<Book>();
+            #region Books
             books.Add(new Book("Harry Potter", "J.K. Rowling", DateTime.ParseExact("2011-03-21", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            books.Add(new Book("Kvinde kend din plads", "Kent Pedersen", DateTime.ParseExact("2020-08-27", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            books.Add(new Book("Minecraft handbook", "Mojang", DateTime.ParseExact("2012-05-17", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            books.Add(new Book("Ida & morgenbrødet, del 1", "Ida Svendsen", DateTime.ParseExact("2020-05-20", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            books.Add(new Book("Ida & morgenbrødet, del 2", "Ida Svendsen", DateTime.ParseExact("2020-06-28", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            books.Add(new Book("Sådan klipper du agurken", "Kent Pedersen", DateTime.ParseExact("1824-04-01", "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+            #endregion
 
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey(true);
+            bool run = true;
+            while (run)
+            {
+                MainMenu();
+                switch (Console.ReadKey(false).KeyChar)
+                {
+                    case '1'://See available books
+                        ShowBooks(false, true);
+                        break;
+                    case '2': //See all books
+                        ShowBooks(true, true);
+                        break;
+                    case '3': //Borrow a book
+                        BorrowABook();
+                        break;
+                    case '4': //Return a book
+                        ReturnABook();
+                        break;
+                    case '5': //Add a new book, requires login
+                        if (Login())
+                        {
+                            AddABook();
+                        }
+                        else
+                        {
+                            Console.WriteLine("You don't have access to add new books, maybe suggest your book idea to a librarian");
+                        }
+                        break;
+                    case '6': //Remove a book, requires login
+                        if (Login())
+                        {
+                            RemoveABook();
+                        }
+                        else
+                        {
+                            Console.WriteLine("You don't have access to add new books, maybe suggest your book idea to a librarian");
+                        }
+                        break;
+                    case '7': //Exit
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        static void MainMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("                 H1 Library System                ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+            Console.WriteLine("(1) See available books");
+            Console.WriteLine("(2) See all books");
+            Console.WriteLine("(3) Borrow a book");
+            Console.WriteLine("(4) Return a book");
+            Console.WriteLine("(5) Add a new book");
+            Console.WriteLine("(6) Remove a new book");
+            Console.WriteLine("(7) Exit");
+            Console.WriteLine();
+            Console.Write("Enter your choice: "); //Prompt Line
+        }
+
+        /// <summary>
+        /// Show books
+        /// </summary>
+        /// <param name="showAllBooks">Show all, or just available books</param>
+        static void ShowBooks(bool showAllBooks, bool wait)
+        {
+            Console.Clear();
+            if (showAllBooks)
+            {
+                Console.WriteLine("==================================================");
+                Console.WriteLine("                   All our books                  ");
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+                foreach (Book book in books)
+                {
+                    if (book.Available)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.WriteLine($"({book.ID}) {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("==================================================");
+                Console.WriteLine("             All our available books              ");
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+                foreach (Book book in books)
+                {
+                    if (book.Available)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"({book.ID}) {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                    }
+                }
+            }
+            if (wait)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                Console.WriteLine("Press any key to return");
+                Console.ReadKey(true);
+            }
+        }
+
+
+        static void BorrowABook()
+        {
+            ShowBooks(false, false);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("               Pick a book to borrow              ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+            Console.Write("Book ID: ");
+            try
+            {
+                uint input = Convert.ToUInt32(Console.ReadLine());
+                foreach (Book book in books)
+                {
+                    if (book.ID == input)
+                    {
+                        booksBorrowed.Add(book);
+                        book.Available = false;
+                        Console.WriteLine($"Successfully borrowed {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                        System.Threading.Thread.Sleep(2000);
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Thats not a valid book id!");
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+
+
+        static void ReturnABook()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            if (booksBorrowed.Count > 0)
+            {
+                foreach (Book book in booksBorrowed)
+                {
+                    Console.WriteLine($"({book.ID}) {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("==================================================");
+                Console.WriteLine("               Pick a book to return              ");
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+                Console.Write("Book ID: ");
+                try
+                {
+                    uint input = Convert.ToUInt32(Console.ReadLine());
+                    foreach (Book book in booksBorrowed)
+                    {
+                        if (book.ID == input)
+                        {
+                            booksBorrowed.Remove(book);
+                            book.Available = true;
+                            Console.WriteLine($"Successfully returned {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                            System.Threading.Thread.Sleep(2000);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Thats not a valid book id!");
+                    System.Threading.Thread.Sleep(2000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You don't have any books to return");
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+
+        static bool Login()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("                       LOGIN                      ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+            Console.Write("Password: ");
+
+            ConsoleKeyInfo key;
+            string passwordInput = null;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                key = Console.ReadKey(true);
+
+                //Backspace should not work here, as its used to delete the last character
+                if (key.Key != ConsoleKey.Backspace)
+                {
+                    passwordInput += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("\b \b"); //\b\b is a backspace keypress
+                }
+            }
+            //Stops recieving keys once enter has been pressed
+            while (key.Key != ConsoleKey.Enter);
+
+            if (passwordInput.Substring(0, passwordInput.Length - 1).Equals("adminPassword"))
+            {
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrect password!");
+                System.Threading.Thread.Sleep(1500);
+            }
+            return false;
+        }
+
+
+        static void AddABook()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("                    Add a book                    ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+
+            Console.Write("Book name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Book author: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Correct format is yyyy-MM-dd, example: 2000-04-12\nBook release date: ");
+            try
+            {
+                DateTime releaseDate = DateTime.ParseExact(Console.ReadLine(), "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                books.Add(new Book(name, author, releaseDate));
+                Console.WriteLine($"Successfully added {name}, by {author} written in {releaseDate}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect format");
+                System.Threading.Thread.Sleep(2000);
+                return;
+            }
+            System.Threading.Thread.Sleep(1500);
+        }
+
+
+        static void RemoveABook()
+        {
+            ShowBooks(true, false);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("                  Remove a book                   ");
+            Console.WriteLine("==================================================");
+            Console.WriteLine();
+            Console.Write("Book id: ");
+            try
+            {
+                uint input = Convert.ToUInt32(Console.ReadLine());
+                foreach (Book book in books)
+                {
+                    if (book.ID == input)
+                    {
+                        books.Remove(book);
+                        Console.WriteLine($"Successfully removed {book.Name} by {book.Author}, written in {book.ReleaseDate}");
+                        System.Threading.Thread.Sleep(2000);
+                        return;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect format");
+                System.Threading.Thread.Sleep(2000);
+                return;
+            }
+            Console.WriteLine("Incorrect ID");
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
